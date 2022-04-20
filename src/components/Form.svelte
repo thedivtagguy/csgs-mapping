47adb156a59a40f2c81b88402e9898c73053050a
Aman Bhargava <amanbhargava2001@gmail.com>
2022-04-20 23:21:20 +0530

    Add aman form

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,4 +1,4 @@
-<form name="contact" method="POST" data-netlify="true">
+<form name="aman" method="POST" data-netlify="true">
     <p>
       <label>Your Name: <input type="text" name="name" /></label>
     </p>

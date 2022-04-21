fa2039715ad4df40b3797d9d5328bde966009a83
Aman Bhargava <amanbhargava2001@gmail.com>
2022-04-21 10:13:38 +0530

    Trial Fix Contact

---
+++ src/components/Contact.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Form from "./Form.svelte";
+    import Menu from "./Menu.svelte";
+</script>
+    
+    <!-- Iterate and show all story headings -->
+    <main class="">
+        <Menu />
+    <Form />
+    
+    </main>
+    
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,12 +1,11 @@
 <script>
     import Meta from "$components/Meta.svelte";
     import Footer from "$components/Footer.svelte";
-    import Form from "$components/Form.svelte";
+    import Contact from "$components/Contact.svelte";
     import Menu from "$components/Menu.svelte";
   </script>
   
   <Meta />
-  <Menu />
-  <Form/>
+  <Contact />
   <Footer />
   

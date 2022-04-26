a4a02b14eee29d4fc3d02778bd6a57714188d2c8
thedivtagguy <amanbhargava2001@gmail.com>
2022-04-26 19:11:00 +0530

    Resizing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,32 +6,11 @@ const header = publications.shift();
 // Create a new JSON object with first 5 rows of data
 const data = publications.slice(0, 5);
 export const  publicationsData = data;
-import Form from "../components/Form.svelte";
 import StackedBar from "../components/charts/StackedBar.svelte";
 </script>
 
 <header class="py-1">
   <div class="py-12 ">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <h2 class="text-lg font-bold leading-6 text-gray-900">
-        Publications
-      </h2>
-      <!-- Render publicationsData -->
-      {#each publicationsData as publication}
-      <div class="mt-4">
-        <div class="flex items-center justify-between">
-          <div class="ml-4">
-            <p class="text-sm leading-5 font-medium text-gray-900">
-              <a href="/" class="hover:underline">{publication.title}</a>
-            </p>
-            <div class="flex text-sm leading-5 text-gray-500">
-              <time datetime="2020-03-16">{publication.genre}</time>
-            </div>
-          </div>
-        </div>
-    </div>
-    {/each}
-    </div>
     <StackedBar/>
   </div>
 </header>
--- src/components/charts/StackedBar.svelte
+++ src/components/charts/StackedBar.svelte
@@ -1,6 +1,11 @@
 <script>
 	import { scaleLinear } from 'd3-scale';
 	import publications from "$data/publications.csv";
+
+	///////////////////////////////////////////////////////////////////
+	// Data Preprocessing /////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////
+
 	const refined = [];
 	// We will keep only year, title, genre
 	const data = publications.map(d => ({
@@ -48,7 +53,7 @@
 
 	// Create an array of unique genres
 	const genres = data.map(d => d.genre).filter((v, i, a) => a.indexOf(v) === i);
-	const genreColors = ["#fd7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "#fdcce5", "#8bd3c7"]
+	const genreColors = ["#392744", "#8E6472", "#E6DA99", "#FC706E", "#08A388", "#DB5526", "#8E4635", "#39081E", "#0E8587"]
 
 	// Iterate through data2 and add an item called color with the associated color for each book
 	data2.forEach(d => {
@@ -58,13 +63,15 @@
 	});
 
 	////////////////////////////////////////////////////////////////////
+	//////// D3 Config /////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////
 
 	const xTicks = [1990, 1995, 2000, 2005, 2010, 2015];
 	const yTicks = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 	const padding = { top: 20, right: 50, bottom: 35, left: 25 };
 
 	let width = 1100;
-	let height = 600;
+	let height = 800;
 
 	$: xScale = scaleLinear()
 		.domain([0, xTicks.length ])
@@ -75,8 +82,14 @@
 		.range([height - padding.bottom, padding.top]);
 
 	$: innerWidth = width - (padding.left + padding.right);
-	$: barWidth = innerWidth / xTicks.length - 152; 
+	$: barWidth = innerWidth / xTicks.length - 150; 
 	
+
+	////////////////////////////////////////////////////////////////////
+	//////// Filters and Hover /////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+
 	$: genreSelection = null;
 
 	// Function to handleFill. 
@@ -98,10 +111,10 @@
 
 <div id="facets" class="flex flex-row">
 	{#each genres as genre}
-		<div class="w-1/3">
+		<div class="w-full">
 			<div class="flex flex-col items-center">
-					<div class="w-full h-full bg-gray-200 rounded-lg shadow-lg">
-						<div on:click={handleClick(genre)} class="text-center text-gray-700 text-xs">{genre}</div>
+					<div  on:click={handleClick(genre)} class="h-[30px]  cursor-pointer w-full bg-gray-200 rounded-lg shadow-lg">
+						<h4 class="text-center text-gray-700 text-xs">{genre}</h4>
 					</div>
 			</div>
 		</div>
@@ -121,7 +134,7 @@
 
 		<g class="axis x-axis">
 			{#each data2 as point, i}
-				<g class="tick" transform="translate({xScale(i)/7 + 5} ,{height})">
+				<g class="tick" transform="translate({xScale(i)/7 + 2} ,{height})">
 					<text x="{barWidth/2}" y="-17">{point[0].year}</text>
 				</g>
 			{/each}
@@ -132,7 +145,7 @@
 				{#each {length: point[0].totalCount} as book, j}
 				<rect class="bars"  
 				fill="{handleFill(point[j])}"
-				x="{xScale(i)/7 + 5}" y="{yScale(j) -8}" width="{barWidth}" height="8px"></rect>
+				x="{xScale(i)/7 + 2}" y="{yScale(j) -8}" width="{barWidth}" height="8"></rect>
 				{/each}
 			{/each}
 			</g>
@@ -154,7 +167,7 @@
 	svg {
 		position: relative;
 		width: 100%;
-		height: 500px;
+		height: 600px;
 	}
 
 	.tick {
@@ -182,7 +195,9 @@
 	}
 
 	.bars rect {
-		stroke: none;
+		stroke: #000000;
+		stroke-width: 0.5px;
 		opacity: 0.65;
+		margin-bottom: 17px;
 	}
 </style>

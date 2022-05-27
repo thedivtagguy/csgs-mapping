05c0238cab040bae9eb2dea243ce8da0648f4f92
PoppadomCo <nithyasubramaniandesign@gmail.com>
2022-05-27 14:07:10 +0530

    Polygon Generator fixed

--- src/components/charts/StackedBar.svelte
+++ src/components/charts/StackedBar.svelte
@@ -184,20 +184,42 @@
 	// Function to generate the polygon
 
 	$: polygonGenerator = (xStartingPos , yStartingPos) => {
+
 		yStartingPos -= 16;
 		// Generate a polygon with random points
-		let polygon = 'M' + xStartingPos + ',' + yStartingPos;
-		let x = xStartingPos ;
-		let y = yStartingPos ;
-		let r = 10;
-		let x2 = 10+ x + r * Math.random(7,8);
-		let y2 =  y - 2 + r * Math.random(7,8);
+
+		let r = 2; //r is the maximum a vertex can vary. Might also need to be a function of screenwidth
+
+		let c1 = r * Math.random();
+		let c2 = r * Math.random();
+		let c3 = r * Math.random();
+		let c4 = r * Math.random();
+
+		
+		let x = xStartingPos + c1;
+		let y = yStartingPos + c1;
+
+		//width (a) and height (b) of rectangle sides. Should ideally be a function of screenwidth?
+		let a = 20;
+		let b = 14;
+
+		let x2 = a + xStartingPos - c2;
+		let y2 =  yStartingPos + c2;
+
+		let x3 = a + xStartingPos - c3;
+		let y3 = b + yStartingPos -c3;
+
+		let x4 = xStartingPos + c4;
+		let y4 = b + yStartingPos -c4;
+
+		let polygon = 'M' + x + ',' + y;
 		polygon += 'L' + x2 + ',' + y2;	
-		polygon += 'L' + x2 + ',' + (y2 +10);
-		polygon += 'L' + x + ',' + (y2 + 8);
-		polygon += 'L' + x + ',' + y2  ;
+		polygon += 'L' + x3 + ',' + y3;
+		polygon += 'L' + x4 + ',' + y4;
+		//polygon += 'L' + x + ',' + y  ;
 		polygon += 'Z';
 		return polygon;
+
 	}
 
 	// $: trapezoidGenerator = (w, h, xStartingPos, yStartingPos) => {

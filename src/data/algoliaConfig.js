55412df4c8a7f36d548d77f60cbc534b133b758b
thedivtagguy <amanbhargava2001@gmail.com>
2022-06-06 22:32:13 +0530

    Add Config Algolia

--- src/data/algoliaConfig.js
+++ src/data/algoliaConfig.js
@@ -4,13 +4,11 @@ async function loadData() {
   // Fetch data from publications.csv and turn it into a JSON array
   const publications = await d3.csv('publications.csv')
     // Turn publications into a JSON array and return it
-    return publications.map(publication => {
-        return {
+    return publications.map((publication) => ({
             ...publication,
             // Create new field called ID and create an ID from the publication's row number
             id: publication.row,
-}
-    }
+    }))
 }
 
 const algoliaConfig = {
--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,32 @@ import sveltePreprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 import { indexAlgolia } from 'svelte-algolia/server-side'
 
+
+import 'dotenv/config' // optional
+import d3 from 'd3'
+async function loadData() {
+  // Fetch data from publications.csv and turn it into a JSON array
+  const publications = await d3.csv('publications.csv')
+    // Turn publications into a JSON array and return it
+    return publications.map((publication) => ({
+            ...publication,
+            // Create new field called ID and create an ID from the publication's row number
+            id: publication.row,
+    }))
+}
+
+const algoliaConfig = {
+  appId: process.env.VITE_ALGOLIA_APP_ID,
+  // don't prefix admin key with VITE_ else it would get exposed to client-side code
+  apiKey: process.env.ALGOLIA_ADMIN_KEY,
+  indices: [
+    { name: `publications`, getData: loadData },
+  ],
+  settings: {
+    attributesToHighlight: [`title`],
+  },
+}
+
 indexAlgolia(algoliaConfig)
 const { thedivtagguy } = JSON.parse(fs.readFileSync("package.json", "utf8"));
 const dev = process.env.NODE_ENV === "development";

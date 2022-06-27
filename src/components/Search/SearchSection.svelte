3871c9c8d02b706fcbe5c62aa13b20b9d9ea3ed4
thedivtagguy <amanbhargava2001@gmail.com>
2022-06-27 19:33:52 +0530

    Multi Indice Search Added

--- src/components/Search/SearchSection.svelte
+++ src/components/Search/SearchSection.svelte
@@ -1,15 +1,62 @@
 <script>
-    import Search from 'svelte-algolia'
-    import Hit from './Hit.svelte'
-  
-    const appId = '8P43BBJQAR'
-    const searchKey = '2c3f24b6adc627d1edf4a3e6879e1e62'
+  import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits, index } from 'instantsearch.js/es/widgets';
+import {onMount} from 'svelte';
+
+const searchClient = algoliasearch('8P43BBJQAR', '2c3f24b6adc627d1edf4a3e6879e1e62');
+
+
+// Add onMount 
+onMount(() => {
+  const search = instantsearch({
+  indexName: 'publications',
+  searchClient,
+  routing: true,
+});
+
+search.addWidgets([
+  searchBox({
+    container: '#searchbox',
+  }),
+
+  hits({
+    container: '#publications-search',
+    templates: {
+      item:
+        '{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}',
+    },
+  }),
+
+  index({ indexName: 'events' })
+    .addWidgets([
+      hits({
+        container: '#events-search',
+        templates: {
+          item:
+            '{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}',
+        },
+      }),
+    ]),
+]);
+
+search.start();
+
+});
+
 </script>
   
-  <header>
-    <Search
-      {appId}
-      {searchKey}
-      indices={{ publications: Hit }}
-      placeholder="Search Index" />
-  </header>
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.3.1/themes/reset-min.css" integrity="sha256-t2ATOGCtAIZNnzER679jwcFcKYfLlw01gli6F6oszk8=" crossorigin="anonymous">
+</svelte:head>
+<div class="ais-InstantSearch">
+  <h1>InstantSearch.js e-commerce demo</h1>
+
+  <div class="right-panel">
+    <div id="searchbox"></div>
+    <div id="publications-search"></div>
+    <div id="events-search"></div>
+    <div id="hits"></div>
+    <div id="pagination"></div>
+  </div>
+</div>

d9f3638d310e79759a7511763de45ea9e9092313
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-02 16:38:07 +0530

    Add Search Page Again

---
+++ src/routes/search.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Footer from "$components/Footer.svelte";
+import Menu from "$components/Menu.svelte";
+  import Meta from "$components/Meta.svelte";
+  import SearchSection from "$components/Search/SearchSection.svelte";
+</script>
+<section>
+  <Meta/>
+  <Menu/>
+  <SearchSection />
+  <Footer />
+</section>

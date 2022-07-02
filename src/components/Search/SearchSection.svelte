0e78e9cf143bbe2c2be6b2aa472356310e0294f9
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-02 22:13:35 +0530

    Results page

--- src/components/Search/SearchSection.svelte
+++ src/components/Search/SearchSection.svelte
@@ -1,11 +1,11 @@
 <script>
 import algoliasearch from 'algoliasearch/lite.js';
 import instantsearch from 'instantsearch.js';
-import { highlight } from 'instantsearch.js/es/helpers';
-import { connectHits } from 'instantsearch.js/es/connectors'
+import { highlight } from 'instantsearch.js/es/helpers/index.js';
+import { connectHits } from 'instantsearch.js/es/connectors/index.js'
 import { searchBox, hits, index } from 'instantsearch.js/es/widgets/index.js';
 import {onMount} from 'svelte';
-import { connectSearchBox } from 'instantsearch.js/es/connectors'
+import { connectSearchBox } from 'instantsearch.js/es/connectors/index.js'
 const searchClient = algoliasearch('8P43BBJQAR', '2c3f24b6adc627d1edf4a3e6879e1e62');
 
 	
@@ -22,13 +22,10 @@ const renderSearchBox = (renderOptions, isFirstRender) => {
     const button = document.createElement('button');
     button.innerHTML = `<svg class="ais-SearchBox-submitIcon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 40 40"> <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path> </svg>`;
 
-    input.addEventListener('input', event => {
+    button.addEventListener('click', event => {
       refine(event.target.value);
     });
 
-    button.addEventListener('click', () => {
-      clear();
-    });
 
     widgetParams.container.appendChild(input);
     widgetParams.container.appendChild(loadingIndicator);
@@ -73,9 +70,13 @@ search.addWidgets([
     templates: {
       item:
         `
-        <h4 class="font-semibold text-2xl">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
-        <p class="hit-description">{{ genre }}</p>
-        <p class="hit-description">{{ year }}</p>
+        <div class="py-4">
+          <h4 class="font-semibold text-2xl">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</h4>
+          <div class="flex flex-row gap-6">
+            <p class="text-sm hit-description">{{ genre }}</p>
+            <p class="text-sm hit-description">{{ year }}</p>
+          </div>
+        </div>
         `,
     },
   }),

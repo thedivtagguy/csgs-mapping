203a100cf24e92bba1d318868000867c72304b0a
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-03 11:44:38 +0530

    Search Page

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,18 +1,5 @@
 <script>
-    import { onMount } from 'svelte';
     let query = '';
-    const executeSearch = (query) => {
-        // URL to go to: /search?publications[query]=queryToSearch
-        const url = `/search?publications[query]=${queryToSearch}`;
-        // Redirect to the url
-        window.location.href = url;
-    }
-    onMount(() => {
-        // When button is clicked, execute the search
-        // When input is submitted, execute the search
-
-    })
-
 </script>
     <nav class="">
         <div class="container px-6 py-3 mx-auto md:flex">
@@ -37,11 +24,10 @@
             <div class="w-full md:flex md:items-center md:justify-end">
               
                
-                <div class="relative">
+                <div class="flex">
                    
-                    <form>
 
-                        <input bind:value={query} type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-[color:var(--color-darker-background)] border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
+                        <input bind:value={query} type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-[color:var(--color-darker-background)] border rounded-md  dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
                         <!-- Submit button -->
                         <a rel="external" href="/search?publications[query]={query}">
                             <div class="bg-[color:#d5d2bf] text-[color:var(--off-white)] w-[40px] h-[40px] m-1 p-1 rounded-lg">
@@ -53,7 +39,6 @@
                                 </svg>
                               </div>
                         </a>
-                    </form>
                 </div>
                 <div class="flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0">
                     <a href="/" class="px-2 py-1 text-sm font-bold text-gray-700 transition-colors duration-200 transform rounded  hover:bg-gray-900 hover:text-gray-100 md:mx-2">Home</a>

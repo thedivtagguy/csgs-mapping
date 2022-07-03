46c2e95e6c016f4e6172c5f3b55b81a5b26205b4
thedivtagguy <amanbhargava2001@gmail.com>
2022-07-03 11:36:23 +0530

    Add Search Bar

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -38,17 +38,21 @@
               
                
                 <div class="relative">
-                    
-                    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                        <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
-                    </span>
+                   
                     <form>
 
                         <input bind:value={query} type="text" class="w-full py-2 pl-10 pr-4 text-gray-700 bg-[color:var(--color-darker-background)] border rounded-md  dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Search">
                         <!-- Submit button -->
-                        <a href="/search?publications[query]={query}">Search</a>
+                        <a rel="external" href="/search?publications[query]={query}">
+                            <div class="bg-[color:#d5d2bf] text-[color:var(--off-white)] w-[40px] h-[40px] m-1 p-1 rounded-lg">
+                                <svg xmlns="http://www.w3.org/2000/svg" stroke="#f0f0f0" width="30" height="30" viewBox="-5 -5 28 28">
+                                  <g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.67" transform="translate(1 1)">
+                                    <circle cx="7.11" cy="7.11" r="7.11"/>
+                                    <path d="M16 16l-3.87-3.87"/>
+                                  </g>
+                                </svg>
+                              </div>
+                        </a>
                     </form>
                 </div>
                 <div class="flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0">

0bc89eb3bbc086589db8f4683aa797fce64dc3a5
Aman Bhargava <amanbhargava2001@gmail.com>
2022-09-04 11:45:29 +0530

    Update Form.svelte

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -65,7 +65,7 @@
 
 				<div>
 					<label for="category" class="blocktext-sm pb-2 font-medium text-gray-900 dark:text-gray-400">Resource Category*</label>
-					<select required id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+					<select required id="category" name="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 					<option selected="">Select category</option>
 					<option value="Institutions/Organization">Institutions/Organization</option>
 					<option value="Publication">Publication</option>

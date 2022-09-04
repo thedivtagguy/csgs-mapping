6e59716971eaf82bc3841ffcca10a777159f2c6b
Aman Bhargava <amanbhargava2001@gmail.com>
2022-09-04 11:23:48 +0530

    Update Form.svelte

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -77,7 +77,7 @@
 
 				<div>
 					<label for="message" class="blocktext-sm pb-2 font-medium text-gray-900 dark:text-gray-400">Optional: If you have any additional comments, we'd love to hear from you</label>
-					<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
+					<textarea id="message" name="comments" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your message..."></textarea>
 					
 				</div>
 			  </div>

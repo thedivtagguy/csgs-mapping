b8d269a81ecf36dcd038ffd02a8e16325cc88827
PoppadomCo <nithyasubramaniandesign@gmail.com>
2022-10-11 10:25:24 +0530

    Some changes to make circles fit better in mobile

--- src/components/charts/Circles.svelte
+++ src/components/charts/Circles.svelte
@@ -137,7 +137,7 @@
     // Check if mobile device
 
     let width = 650;
-    let widthMobile = 350;
+    let widthMobile = 360;
     let height = 650;
     let dividedBy = 2;
     let avoidOverlapRadius = 28;
@@ -146,12 +146,12 @@
 
     if (mobileCheck()) {
       width = widthMobile;
-      height = window.innerHeight / 1.4;
-      dividedBy = 2;
+      height = window.innerHeight / 1.3;
+      dividedBy = 3;
       avoidOverlapRadius = 23;
       nodeRadius = 45;
 
-      defaultRange = [0, 10, 30, 50, 70, 90, 110, 130, 150, 190, 210, 230, 250];
+      defaultRange = [0, 10, 30, 50, 70, 90, 110, 130, 150, 190, 210];
     }
 
     let svg;

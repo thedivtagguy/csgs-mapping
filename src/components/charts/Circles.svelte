2ac0a23ecd98a23dc0bd31c016a66e7fcf45c971
PoppadomCo <nithyasubramaniandesign@gmail.com>
2022-10-11 10:33:42 +0530

    changed defaultRange

--- src/components/charts/Circles.svelte
+++ src/components/charts/Circles.svelte
@@ -151,7 +151,7 @@
       avoidOverlapRadius = 23;
       nodeRadius = 45;
 
-      defaultRange = [ 30, 50, 70, 90, 110, 130, 150, 190, 210, 230, 250];
+      defaultRange = [ 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180, 195];
     }
 
     let svg;

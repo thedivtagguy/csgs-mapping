efe6ab49de5bab7da0ecc2217ccef14dec3494d5
PoppadomCo <nithyasubramaniandesign@gmail.com>
2022-11-03 09:21:43 +0530

    Events button padding changed

--- src/components/Events.svelte
+++ src/components/Events.svelte
@@ -77,7 +77,7 @@
      </div>
      <div class="flex justify-end">
          <a href="/events">
-            <button class="bg-[color:var(--color-turquoise)] mx-4 hover:bg-[color:var(--color-green)] text-gray-700 font-bold py-4 px-4 " onclick="search.start()">
+            <button class="bg-[color:var(--color-turquoise)] mx-4 hover:bg-[color:var(--color-green)] text-gray-700 font-bold py-2 px-4 " onclick="search.start()">
               See All Events
             </button>
           </a>
@@ -124,7 +124,7 @@
     .flex {
     display: flex;
     display: -webkit-flex;
-    }
+        }
 
     .main {
         overflow-x: auto;

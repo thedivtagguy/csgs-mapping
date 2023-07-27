5ffd668d16c6da94ef8a876e38a2be3ee79f659e
PoppadomCo <nithyasubramaniandesign@gmail.com>
2023-07-27 10:22:03 +0530

    Added more buttons. Fixed heights

--- src/components/charts/Judgements.svelte
+++ src/components/charts/Judgements.svelte
@@ -17,162 +17,96 @@
           Click on {title} to know more
         </p>
       </div>
-      <!--
-          <div class = "timelines">
-            <svg>
-          <line
-            
-            x1="8"
-            y1="734"
-            x2="8"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="71"
-            y1="734"
-            x2="71"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="141"
-            y1="734"
-            x2="141"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="358"
-            y1="734"
-            x2="358"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="782"
-            y1="734"
-            x2="782"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="1499"
-            y1="734"
-            x2="1499"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-          <line
-            
-            x1="3102"
-            y1="734"
-            x2="3102"
-            y2="1.31134e-07"
-            stroke="#F7B289"
-            stroke-width="2"
-            stroke-dasharray="6 6"
-          />
-        </svg>
-        
-      </div> -->
-      <div class = "background-Lines">
-        <object class ="background_lines" data="/assets/lines.svg"/>
+
+      <div class="background-Lines">
+        <object class="background_lines" data="/assets/lines.svg" />
       </div>
 
-      
-
-      
       <div class="button button1">
-        <!-- svelte-ignore a11y-missing-attribute -->
-
-        <object class ="Punjab_Haryana1" data="/assets/Punjab_Haryana.svg" />
+        <object class="Punjab_Haryana1" data="/assets/Punjab_Haryana.svg" />
         <p class="leading-4 text-sm">Tirath Kaur vs Kirpal Singh</p>
       </div>
-      <div class="button button2">
-        <!-- svelte-ignore a11y-missing-attribute -->
 
+      <div class="button button2">
         <object class="SC1" data="/assets/SC.svg" />
         <p class="leading-4 text-sm">Tuka Ram And Anr vs State of Maharashtra</p>
       </div>
 
-      
+      <div class="button button3">
+        <object class="SC2" data="/assets/SC.svg" />
+        <p class="leading-4 text-sm">Fazal Rab Choudhary vs State of Bihar</p>
+      </div>
+
+      <div class="button button4">
+        <object class="Punjab_Haryana2" data="/assets/Punjab_Haryana.svg" />
+        <p class="leading-4 text-sm">Smt Harvinder Kaur vs Harmander Singh Choudhry</p>
+      </div>
     </div>
   </section>
 </main>
 
-
 <style>
-  
-  
   .container {
-    
     width: 5000px;
-    height: 750px;
+    height: 780px;
     position: relative;
     display: grid;
     grid-template-columns: repeat(63, 80px);
-    grid-template-rows: repeat(10, 75px);
-    overflow-y: scroll;
+    grid-template-rows: repeat(10, 78px);
   }
-  .heading{
-    grid-area: 1/ 1/ 3/ 50;
+  .heading {
+    grid-area: 1/ 1/ 2/ 50;
     z-index: 10;
   }
-  
+
   svg {
     display: block;
     overflow: visible;
   }
 
-.button1{
-  grid-area: 4/1/4/1;
+  .button1 {
+    padding-left: 15px;
+    grid-area: 7/1/8/2;
   }
-  .button2{
-  grid-area: 4/3/5/4;
+  .button2 {
+    grid-area: 4/2/4/4;
   }
+
+  .button3 {
+    padding-right: 15px;
+    grid-area: 7/3/7/4;
+  }
+
+  .button4 {
+    padding-top: 20px;
+  padding-left: 25px;
+    grid-area: 2/3/2/5;
+  }
+
   .background-Lines {
-    
     z-index: 1;
-    grid-area: 1/ 1/ 10/ 63;
+    grid-area: 2/ 1/ 10/ 63;
   }
   .button:hover {
     -ms-transform: scale(1.1em);
     -webkit-transform: scale(1.1);
     tansform: scale(1.1);
   }
-  .button object:hover {
+  /*.button object:hover {
     filter: drop-shadow(-1px -1px 0px #3a3a3a) drop-shadow(1px -1px 0px #3a3a3a)
       drop-shadow(1px 1px 0px #3a3a3a) drop-shadow(-1px 1px 0px #3a3a3a);
-  }
-  
+  }*/
+
   .button {
     z-index: 13;
     color: #3a3a3a;
     text-align: center;
     font-family: Roboto;
-    font-size: 12px;
+    font-size: 10px;
     line-height: 0.6rem;
     font-weight: 400;
   }
-  .button object{
+  .button object {
     display: inline;
     vertical-align: middle;
   }

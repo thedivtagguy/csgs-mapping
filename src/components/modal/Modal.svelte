7826ea5b3645dff99eab5ed74f8b1eb0220f4b62
nithya13 <nithya13@gmail.com>
2025-04-30 10:52:40 +0530

    Modals links added

--- src/components/modal/Modal.svelte
+++ src/components/modal/Modal.svelte
@@ -95,8 +95,35 @@
                   <p class="font-bold text-gray-700 text-base">Political rationale: <span class="font-normal">{data.rationale}</span></p>
                   {/if}
                   {#if data.links}
-                 <a href = "{data.links}" target="_blank" rel="external" class ="hover:text-gray-400"><p class="font-bold text-blue-500 text-base">View the performance</p></a>
-                  {/if}
+                  <a
+                    href="{data.links}"
+                    target="_blank"
+                    rel="external"
+                    class="inline-flex flex-none items-center gap-2 w-auto max-w-max bg-[#A8DCC6] text-gray-700 font-bold px-4 py-2 hover:bg-[#A8DCff] transition"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                      />
+                    </svg>
+                    View the performance
+                  </a>
+                {/if}
                 </div>
             </div>
              
